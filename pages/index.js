8624d508f4573c82cb096a60242c2cf4a35a8463
Matheus Aguiar <134005250+MatheusSilvaAguiarDev@users.noreply.github.com>
2023-11-06 23:56:13 +0000

    adiciona scripts `lint:check`e`lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home (){
-    return <h1>Treino de GitHub Pista Lenta do dia 7   </h1>
+function Home() {
+  return <h1>Treino de GitHub Pista Lenta do dia 7 </h1>;
 }
 
 export default Home;
